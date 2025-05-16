487d6a5ac2e85e758268ace6dea722120f79a21a
alberto <albertopuet8@gmail.com>
2025-05-15 21:52:01 -0300

    commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://raulpuet.com', // o tu dominio sin tilde si usás uno alternativo
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
